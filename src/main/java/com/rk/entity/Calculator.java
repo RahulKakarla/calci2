a01ec731aed23dd9380670a203d26cf83f7f71a8
RahulKakarla <kakarla356@gmail.com>
2017-07-14 13:25:11 -0500

    first

---
+++ src/main/java/com/rk/entity/Calculator.java
@@ -0,0 +1,15 @@
+package com.rk.entity;
+
+public class Calculator {
+	
+	public int add(int x,int y)
+	{
+		return x+y;
+	}
+	public int sub(int x,int y){
+		return x-y;
+	}
+	public int multiply(int x, int y){
+		return x*y;
+	}
+}
---
+++ src/test/java/com/rk/entity/CalculatorTest.java
@@ -0,0 +1,17 @@
+package com.rk.entity;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+	Calculator c=new Calculator();
+	
+	public void testadd(){
+		assertTrue(c.add(5, 4)==9);
+	}
+	public void testMultiply(){
+		assertTrue(c.multiply(4, 5)==20);
+	}
+	public void testSub(){
+		assertTrue(c.sub(9,5)==4);
+	}
+}
